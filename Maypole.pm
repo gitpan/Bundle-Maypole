90e3a96e12e101770dd615044fc4e1cb0b625bc4
Sebastian Riedel <kraihx@googlemail.com>
2004-10-23 19:22:46 +0000

    Import of SRI/Bundle-Maypole-0.01 from CPAN.
    gitpan-cpan-distribution: Bundle-Maypole
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         SRI/Bundle-Maypole-0.01.tar.gz
    gitpan-cpan-author:       SRI
    gitpan-cpan-maturity:     released

---
+++ Maypole.pm
@@ -0,0 +1,54 @@
+package Bundle::Maypole;
+
+use strict;
+
+our $VERSION = '0.01';
+
+=head1 NAME
+
+Bundle::Maypole - All Maypole goodness
+
+=head1 SYNOPSIS
+
+C<perl -MCPAN -e 'install Bundle::Maypole'>
+
+=head1 DESCRIPTION
+
+Installs all the Maypole modules for you.
+
+=head1 CONTENTS
+
+Maypole
+
+Maypole::View::Mason
+
+Maypole::Plugin::Authentication-Abstract
+
+Maypole::Plugin::Component
+
+Maypole::Plugin::Config-YAML
+
+Maypole::Plugin::Exception
+
+Maypole::Plugin::I18N
+
+Maypole::Plugin::Loader
+
+Maypole::Plugin::Relationship
+
+Maypole::Plugin::Transaction
+
+Maypole::Plugin::Untaint
+
+=head1 AUTHOR
+
+Sebastian Riedel, C<sri@oook.de>
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify it under
+the same terms as perl itself.
+
+=cut
+
+1;
